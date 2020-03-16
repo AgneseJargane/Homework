b6a0a269b83db03397b912096ad69dec015d193b
Agucis <agnesej97@gmail.com>
2020-03-16 16:48:03 +0200

    settings

---
+++ homework-server/src/main/java/homework/HomeworkApplication.java
@@ -0,0 +1,23 @@
+package homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class HomeworkApplication  extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkApplication.class, args);
+	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HomeworkApplication.class);
+	}
+
+
+}
